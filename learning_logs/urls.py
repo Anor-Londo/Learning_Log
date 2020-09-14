70f91301d0e38027e5f50f749e7172c6b0ac6e26
Bubble_Interface <Mishareznik@yandex.ru>
2020-09-15 01:32:38 +0300

    added form for adding new topics

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -11,5 +11,7 @@ urlpatterns = [
     # Show all topics
     path('topics/', views.topics, name='topics'),
     # Detail page for a single topic
-    path('topic/(?P<topic_id>\d+)/', views.topic, name='topic')
+    path('topic/(<topic_id>)/', views.topic, name='topic'),
+    # Page for adding a new topic
+    path('new_topic/', views.new_topic, name='new_topic')
 ]
