fcfb1bb4407f384d260db2c65203c5d119142774
Bubble_Interface <Mishareznik@yandex.ru>
2020-09-15 02:28:11 +0300

    added page for adding new entries

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -11,7 +11,9 @@ urlpatterns = [
     # Show all topics
     path('topics/', views.topics, name='topics'),
     # Detail page for a single topic
-    path('topic/(<topic_id>)/', views.topic, name='topic'),
+    path('topic/<topic_id>/', views.topic, name='topic'),
     # Page for adding a new topic
-    path('new_topic/', views.new_topic, name='new_topic')
+    path('new_topic/', views.new_topic, name='new_topic'),
+    # Page for adding a new topic
+    path('new_entry/<topic_id>', views.new_entry, name='new_entry')
 ]
