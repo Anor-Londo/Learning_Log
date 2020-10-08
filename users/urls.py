0e44828c2090428126f1d0c8d6828599cd718c49
Bubble_Interface <Mishareznik@yandex.ru>
2020-10-08 18:25:45 +0300

    added registration

--- users/urls.py
+++ users/urls.py
@@ -11,5 +11,7 @@ urlpatterns = [
     # Login page
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     # Logout page
-    path('logout/', views.logout_view, name='logout')
+    path('logout/', views.logout_view, name='logout'),
+    #Registration page
+    path('register/', views.register, name='register')
 ]
